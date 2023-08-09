9a3360aee2db634d88382e95b17bbd3d3185c94e
Uniworker <markyatsyn23@gmail.con>
2023-08-10 02:35:04 +0300

    Fix the main menu button

--- build/app.js
+++ build/app.js
@@ -1,21 +1,5 @@
 "use strict";
 
-require("core-js/modules/es.array.slice.js");
-require("core-js/modules/es.object.to-string.js");
-require("core-js/modules/es.regexp.to-string.js");
-require("core-js/modules/es.function.name.js");
-require("core-js/modules/es.array.from.js");
-require("core-js/modules/es.string.iterator.js");
-require("core-js/modules/es.regexp.exec.js");
-require("core-js/modules/es.regexp.test.js");
-require("core-js/modules/es.symbol.js");
-require("core-js/modules/es.symbol.description.js");
-require("core-js/modules/es.symbol.iterator.js");
-require("core-js/modules/es.array.iterator.js");
-require("core-js/modules/web.dom-collections.iterator.js");
-function _createForOfIteratorHelper(o, allowArrayLike) { var it = typeof Symbol !== "undefined" && o[Symbol.iterator] || o["@@iterator"]; if (!it) { if (Array.isArray(o) || (it = _unsupportedIterableToArray(o)) || allowArrayLike && o && typeof o.length === "number") { if (it) o = it; var i = 0; var F = function F() {}; return { s: F, n: function n() { if (i >= o.length) return { done: true }; return { done: false, value: o[i++] }; }, e: function e(_e) { throw _e; }, f: F }; } throw new TypeError("Invalid attempt to iterate non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method."); } var normalCompletion = true, didErr = false, err; return { s: function s() { it = it.call(o); }, n: function n() { var step = it.next(); normalCompletion = step.done; return step; }, e: function e(_e2) { didErr = true; err = _e2; }, f: function f() { try { if (!normalCompletion && it.return != null) it.return(); } finally { if (didErr) throw err; } } }; }
-function _unsupportedIterableToArray(o, minLen) { if (!o) return; if (typeof o === "string") return _arrayLikeToArray(o, minLen); var n = Object.prototype.toString.call(o).slice(8, -1); if (n === "Object" && o.constructor) n = o.constructor.name; if (n === "Map" || n === "Set") return Array.from(o); if (n === "Arguments" || /^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(n)) return _arrayLikeToArray(o, minLen); }
-function _arrayLikeToArray(arr, len) { if (len == null || len > arr.length) len = arr.length; for (var i = 0, arr2 = new Array(len); i < len; i++) arr2[i] = arr[i]; return arr2; }
 window.onload = function () {
   var audio = document.getElementById('sound');
   var menu = document.getElementById('menu');
@@ -24,18 +8,22 @@ window.onload = function () {
   menu.addEventListener('click', function (e) {
     e.target.classList.toggle('--closed');
     e.target.classList.toggle('--open');
-    var _iterator = _createForOfIteratorHelper(tools),
-      _step;
-    try {
-      for (_iterator.s(); !(_step = _iterator.n()).done;) {
-        var unit = _step.value;
-        var distance = unit * (360 / tools.length);
-        e.target.classList.contains('--open') ? setTrigger(unit, distance) : setTrigger(unit, '-360');
+    var _loop = function _loop(i) {
+      var distance = i * (360 / tools.length);
+      if (e.target.classList.contains('--open')) {
+        tools[i].firstChild.classList.remove('sr-only');
+        tools[i].firstChild.style.cssText = 'height:64px;width:64px;margin-left:-200px;animation: pulse 16s ease-in-out infinite;';
+        setTrigger(tools[i], distance);
+      } else {
+        setTrigger(tools[i], '-360');
+        tools[i].firstChild.style.color = 'transparent';
+        setTimeout(function () {
+          tools[i].firstChild.classList.add('sr-only');
+        }, 500);
       }
-    } catch (err) {
-      _iterator.e(err);
-    } finally {
-      _iterator.f();
+    };
+    for (var i = 0; i < tools.length; i++) {
+      _loop(i);
     }
   });
   function setTrigger(item, longitude) {
--- js/app.js
+++ js/app.js
@@ -7,12 +7,21 @@ window.onload = function() {
   menu.addEventListener('click', (e)=>{
     e.target.classList.toggle('--closed')
     e.target.classList.toggle('--open')
-    for (let unit of tools) {
-      let distance = unit * (360 / tools.length)
-      e.target.classList.contains('--open') ? setTrigger(unit, distance) : setTrigger(unit, '-360')
+    for (let i=0; i<tools.length; i++) {
+      let distance = i * (360 / tools.length)
+      if(e.target.classList.contains('--open')) {
+        tools[i].firstChild.classList.remove('sr-only')
+        tools[i].firstChild.style.cssText = 'height:64px;width:64px;margin-left:-200px;animation: pulse 16s ease-in-out infinite;'
+        setTrigger(tools[i], distance)
+      } else {
+        setTrigger(tools[i], '-360')
+        tools[i].firstChild.style.color = 'transparent'
+        setTimeout(() => {
+          tools[i].firstChild.classList.add('sr-only')
+        }, 500);
+      }
     }
   })
-
   function setTrigger(item, longitude) {
     item.style.transform = `rotate(${longitude}deg)`
     item.querySelector('a').style.transform = `rotate(-${longitude}deg)`
