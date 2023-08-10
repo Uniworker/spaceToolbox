b1eb806243812c5f50e7a7ee6138bc0be67b4c30
Uniworker <markyatsyn23@gmail.con>
2023-08-11 02:56:12 +0300

    Secodary pages fix

--- js/notes.js
+++ js/notes.js
@@ -2,26 +2,26 @@ window.onload = function () {
   let prevData = ''
   const text = document.getElementById("title")
   const counter = document.createElement('span')
-  counter.style.cssText = 'position:absolute;top:17%;left:70%;color:gray;font-size:14px;z-index:2'
+  counter.style.cssText = 'position:absolute;top:11%;left:70%;color:gray;font-size:14px;z-index:3'
   document.body.append(counter)
   text.addEventListener('input', () => {
     counter.textContent = `${text.value.length}/${text.maxLength}`
   })
   document.addEventListener('click', (e) => {
-    if (e.target.classList.contains('form-control')) {
+    if (e.target.classList.contains('main__field')) {
       inputHandler('', 'inherit', '')
       counter.textContent = `${text.value.length}/${text.maxLength}`
       text.classList.remove('active')
     }
-    if (e.target.classList.contains('btn-outline-info')) {
+    if (e.target.classList.contains('main__btn')) {
       if (text.value !== '' && text.value !== prevData) {
         document.getElementById('list').insertAdjacentHTML('beforeend', `<li
-          class="list-group-item d-flex justify-content-between align-items-center" style="border-radius: 8px;"
+          class="list__item flex-grid" style="border-radius: 8px;"
         >
-          <span id="content">${text.value}</span>
-          <span>
-            <span class="btn btn-small btn-success" id="editor" title="Edit the note">&#128393;</span>
-            <span class="btn btn-small btn-danger" title="Delete forever">&times;</span>
+          <span id="content" class="flex-grid__col">${text.value}</span>
+          <span class="flex-grid__col flex-grid__col--sm">
+            <span class="btn btn--success" id="editor" title="Edit the note"">&#128393;</span>
+            <span class="btn btn--danger" title="Delete forever">&times;</span>
           </span>
         </li>`)
         prevData = text.value
@@ -32,14 +32,14 @@ window.onload = function () {
         text.classList.add('active')
       }
     }
-    if (e.target.classList.contains('btn-success')) {
+    if (e.target.classList.contains('btn--success')) {
       editContent(document.getElementById('editor'), document.getElementById('content'), true)
     } else {
-      if (e.target.classList.contains('btn-warning')) {
+      if (e.target.classList.contains('btn--warning')) {
         editContent(document.getElementById('editor'), document.getElementById('content'))
       }
     }
-    if (e.target.classList.contains('btn-danger')) document.querySelector('.list-group-item').remove()
+    if (e.target.classList.contains('btn--danger')) document.querySelector('.list__item').remove()
   })
   function inputHandler(data, view, hint) {
     text.value = data
@@ -48,17 +48,17 @@ window.onload = function () {
   }
   function editContent(editor, content, state = false) {
     if (state) {
-      editor.classList.add('btn-warning')
+      editor.classList.add('btn--warning')
       editor.innerHTML = '&#10004;'
       content.setAttribute('contenteditable', 'true')
       content.style.cssText = 'border-bottom:2px dashed red;font-style:italic'
-      editor.classList.remove('btn-success')
+      editor.classList.remove('btn--success')
     } else {
-      editor.classList.add('btn-success')
+      editor.classList.add('btn--success')
       editor.innerHTML = '&#128393;'
       content.setAttribute('contenteditable', 'false')
       content.style.cssText = 'border-bottom:none;font-style:normal'
-      editor.classList.remove('btn-warning')
+      editor.classList.remove('btn--warning')
     }
   }
 }
